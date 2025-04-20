acee5586f0ec7e5986d40d134cdb5105800865f6
Zeinab Ganjei <zzganjei@gmail.com>
2025-04-20 11:36:04 +0200

    removed unused lib

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,5 @@
 "use client";
 import {useState} from "react";
-import { notFound } from "next/navigation";
 
 export default function UploadPage(){
     const [file, setFile] = useState<File | null>(null);
